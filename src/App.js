3eaa4d9c2aa0fadd05ff1e01afd498df97d04d7c
Daniel Valencia <dfvalenciav@gmail.com>
2021-05-26 19:06:44 -0500

    05 Assigment 1

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
           </div>
         </Navbar>
         <Menu dishes = {this.state.dishes}/>
-      </div>
+        </div>
     );
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay,  CardTitle} from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -14,26 +15,7 @@ class Menu extends Component {
         this.setState({selectedDish: dish}); 
     }
 
-    renderDish(dish) {
-        if(dish != null){
-            return (
-            <div className="col-12">
-            <Card>
-                <CardImg  width="100%" src = {dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-            </div>
-            );
-        }
-        else{
-            return (
-                <div></div>
-            );
-        }
-    }
+
 
     render (){
         
@@ -56,7 +38,8 @@ class Menu extends Component {
                         {menu}
                 </div>
                 <div className = "row">
-                    {this.renderDish(this.state.selectedDish)}
+                <Dishdetail dish = {this.state.selectedDish}/>
+                {/*{this.renderDish(this.state.selectedDish)}*/}
                 </div>
             </div>
         );        
