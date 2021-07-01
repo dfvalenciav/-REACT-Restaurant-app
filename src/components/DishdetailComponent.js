af316b46587e652103b4309fe7764f5ae4395d24
Daniel Valencia <dfvalenciav@gmail.com>
2021-06-30 20:42:47 -0500

    21 fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends Component{
   handleSubmit(values) {
     /*console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));*/
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -106,7 +106,7 @@ function RenderDish({dish}) {
             );
     }
 
-  function  RenderComments({comments, addComment, dishId}){
+  function  RenderComments({comments, postComment, dishId}){
         if (comments != null) {
             return (
             <div className='col-12 col-md-5 m-1'>
@@ -128,7 +128,7 @@ function RenderDish({dish}) {
                     );
                     })}
                 </ul>
-                <CommentForm dishId = {dishId} addComment={addComment} />
+                <CommentForm dishId = {dishId} postComment={postComment} />
             </div>
             );
         }
@@ -178,7 +178,7 @@ function RenderDish({dish}) {
                     <div className ="row">
                         <RenderDish dish = {props.dish}/>
                         <RenderComments comments = {props.comments}
-                            addComment = {props.addComment}
+                            postComment = {props.postComment}
                             dishId = {props.dish.id}/>                     
                     </div>
                 </div>
